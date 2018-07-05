818186e36f39de8557dd4046e38d8367c2d87eff
Marcelo Dominguez <marcelo@mimiquate.com>
2018-07-05 16:59:44 -0300

    Add ability to remove subscriptions from Subscribed page

--- ext/src/inject.js
+++ ext/src/inject.js
@@ -25,9 +25,7 @@ window.listSubscribed = (() => {
     a.innerHTML = `<span class="count"></span>Subscribed`;
     document.getElementsByClassName("filter-list")[0].append(a);
 
-    chrome.storage.local.get(null, function(items) {
-      document.querySelector(".js-subscribed .count").innerHTML = Object.keys(items).length;
-    });
+    addCountToSubscribedLink();
 
     a.addEventListener("click", function(e) {
       if (!e.target.classList.contains("selected")) {
@@ -60,12 +58,44 @@ window.listSubscribed = (() => {
     });
   }
 
+  function addCountToSubscribedLink() {
+    chrome.storage.local.get(null, function(items) {
+      document.querySelector(".js-subscribed .count").innerHTML = Object.keys(items).length;
+    });
+  }
+
+  function destroyRepoBox(e) {
+    var repoBox = e.target.closest(".notifications-list");
+
+    chrome.storage.local.get(null, function(items) {
+      Object.keys(items).forEach(function (key) {
+        var item = items[key];
+        if (item.repo === repoBox.dataset.repo) {
+          chrome.storage.local.remove([key]);
+        }
+      });
+
+      addCountToSubscribedLink();
+    });
+
+    repoBox.remove();
+  }
+
   function addRepoBox(item) {
-    if (document.querySelector(`[data-repo="${item.repo}"]`) === null) {
+    if (document.querySelector(`.notifications-list[data-repo="${item.repo}"]`) === null) {
       var innerText, div;
-      innerText = `<div class="notifications-list">
+      innerText = `<div class="notifications-list" data-repo="${item.repo}">
                      <div class="boxed-group flush js-notifications-browser">
-                       <h3><a href="https://github.com/${item.repo}" data-repo="${item.repo}" class="css-truncate css-truncate-target notifications-repo-link">${item.repo}</a></h3>
+                       <h3>
+                         <a href="https://github.com/${item.repo}" data-repo="${item.repo}" class="css-truncate css-truncate-target notifications-repo-link">${item.repo}</a>
+                         <div class="notification-actions">
+                         <button class="btn-link js-gs-destroy">
+                          <svg class="octicon octicon-trashcan" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"></path>
+                          </svg>
+                         </button>
+                         </div>
+                       </h3>
                        <ul class="boxed-group-inner list-group notifications" data-repo="${item.repo}"></ul>
                      </div>
                    </div>`;
@@ -73,6 +103,7 @@ window.listSubscribed = (() => {
       div = document.createElement('div');
       div.innerHTML = innerText;
 
+      div.querySelector(".js-gs-destroy").addEventListener("click", destroyRepoBox);
       document.querySelector(".col-9.float-right").appendChild(div.firstChild);
     }
   }
